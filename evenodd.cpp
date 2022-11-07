ba128ed7036bd2a8a15b2d718c3f7ee2703766e1
yorkking <2410613187@qq.com>
2022-11-07 13:50:43 +0000

    add basic read

--- evenodd.cpp
+++ evenodd.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define PATH_MAX_LEN 60
+#define PATH_MAX_LEN 512
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
 #define BYTE_TO_BINARY(byte)                                                   \
@@ -190,6 +190,51 @@ RC encode(const char *path, int p) {
   return RC::SUCCESS;
 }
 
+/*
+ * just read from 0 ~ p-1 to recover file
+ */
+RC basicRead(const char *path, const char *save_as, int p) {
+  const char *filename = basename(path);
+  char output_path[PATH_MAX_LEN];
+  int write_fd = open(save_as, O_CREAT | O_WRONLY, S_IRWXU);
+  if (write_fd < 0) {
+    perror("Error:  can't create file !");
+    return RC::FILE_NOT_EXIST;
+  }
+
+  int buffer_size = 4 * 1024 * 1024;
+  char *buffer = new char[buffer_size];
+  for (int i = 0; i < p; i++) {
+    sprintf(output_path, "./disk_%d", i);
+    if (stat(output_path, &st) == -1) {
+      perror("Error: directory doesn't exist !");
+      return RC::FILE_NOT_EXIST;
+    }
+    sprintf(output_path, "./disk_%d/%s_%d", i, filename, p);
+    int fd = open(output_path, O_RDONLY);
+    if (fd < 0) {
+      perror("Error: can't open such file!");
+      return RC::FILE_NOT_EXIST;
+    }
+
+    int read_size = 0;
+    do {
+      read_size = read(fd, buffer, buffer_size);
+      if (read_size < 0) {
+        perror("can't read");
+      }
+      int write_size = write(write_fd, buffer, read_size);
+      if (write_size < 0) {
+        perror("can't write!");
+      }
+      // printf("read size: %d, write size: %d\n", read_size, write_size);
+    } while (read_size == buffer_size);
+    close(fd);
+  }
+  close(write_fd);
+  return RC::SUCCESS;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     usage();
@@ -217,6 +262,11 @@ int main(int argc, char **argv) {
      * before), and "save_as" is "tmp_file". After the read operation, there
      * should be a file named "tmp_file", which is the same as "testfile".
      */
+    char *filename = argv[2];
+    char *save_as = argv[3];
+    int p = atoi(argv[4]);
+    RC rc = basicRead(filename, save_as, p);
+
   } else if (strcmp(op, "repair") == 0) {
     /*
      * Please repair failed disks. The number of failures is specified by
@@ -230,112 +280,4 @@ int main(int argc, char **argv) {
     printf("Non-supported operations!\n");
   }
   return 0;
-}
-
-void myWrite(char *file_path, int p) {
-  if (p <= 0) {
-    printf("p should greater than 0 but p = %d", p);
-    return;
-  }
-
-  /* init */
-  // 假定输入为大小 (p-1)*p, 即有 p 列，每列有 p-1 个元素
-  char **data = (char **)malloc(sizeof(char *) * p);
-  for (int i = 0; i < p; i++) {
-    data[i] = (char *)malloc(sizeof(char) * (p - 1));
-    memset(data[i], 0, p - 1);
-  }
-  char *row_parity = (char *)malloc(sizeof(char) * (p - 1));
-  char *diagonal_parity = (char *)malloc(sizeof(char) * (p - 1));
-
-  /* read input file */
-  FILE *input = fopen(file_path, "rb");
-  if (input == NULL) {
-    printf("can't find the file %s", file_path);
-    return;
-  }
-  int num = 0;
-  while (num < p && fread(data[num], sizeof(char), p - 1, input)) {
-    num++;
-  }
-  fclose(input);
-
-  // check input data
-  // for(int i = 0;i < p;i++){
-  //     printf("column %d:", i);
-  //     for(int j = 0;j < p-1;j++){
-  //         printf(BYTE_TO_BINARY_PATTERN",", BYTE_TO_BINARY(data[i][j]));
-  //     }
-  //     printf("\n");
-  // }
-
-  /* encoding */
-  int S = 0;
-  for (int i = 0; i < p; i++) {
-    for (int j = 0; j < p - 1; j++) {
-      row_parity[j] ^= data[i][j];
-      if (i + j == p - 1) {
-        S ^= data[i][j];
-      } else if (i + j < p - 1) {
-        diagonal_parity[i + j] ^= data[i][j];
-      } else {
-        diagonal_parity[i + j - p] ^= data[i][j];
-      }
-    }
-  }
-  for (int i = 0; i < p - 1; i++) {
-    diagonal_parity[i] ^= S;
-  }
-
-  // just for test: output encoding column
-  printf("row parity:");
-  for (int i = 0; i < p - 1; i++) {
-    printf(BYTE_TO_BINARY_PATTERN ",", BYTE_TO_BINARY(row_parity[i]));
-  }
-  printf("\ndiagonal parity:");
-  for (int i = 0; i < p - 1; i++) {
-    printf(BYTE_TO_BINARY_PATTERN ",", BYTE_TO_BINARY(diagonal_parity[i]));
-  }
-  printf("\n");
-
-  /* write to disk */
-  char *filename = basename(file_path);
-  FILE *output;
-  char output_path[PATH_MAX_LEN];
-  size_t write_count = 0;
-  for (int i = 0; i < p + 2; i++) {
-    // create directory if not exist
-    sprintf(output_path, "./disk/disk_%d", i);
-    if (stat(output_path, &st) == -1) {
-      mkdir(output_path, 0700); // mode 0700 = read,write,execute only for owner
-    }
-
-    // open file
-    sprintf(output_path, "./disk/disk_%d/%s_%d", i, filename, i);
-    output = fopen(output_path, "wb");
-    if (!output) {
-      perror("Error in creating file");
-      exit(1);
-    }
-
-    // write file
-    char *ptr;
-    if (i < p) {
-      ptr = data[i];
-    } else if (i == p) {
-      ptr = row_parity;
-    } else {
-      ptr = diagonal_parity;
-    }
-    write_count += fwrite(ptr, sizeof(char), p - 1, output);
-    fclose(output);
-  }
-
-  printf("Totally write %ld elements\n", write_count);
-
-  for (int i = 0; i < p; i++)
-    free(data[i]);
-  free(data);
-  free(row_parity);
-  free(diagonal_parity);
 }
