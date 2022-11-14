6bd4dd6ab22649c627cc4acb59d334973ed28c87
yorkking <2410613187@qq.com>
2022-11-14 13:20:59 +0000

    multiple threads

--- log.cpp
+++ log.cpp
@@ -4,7 +4,7 @@
 
 #define ONE_KILO 1024
 
-static LOG_LEVEL CUR_LEVEL = LOG_LEVEL::LOG_LEVEL_INFO;
+static LOG_LEVEL CUR_LEVEL = LOG_LEVEL::LOG_LEVEL_DEBUG;
 
 int __log_output__(LOG_LEVEL level, const char *file_name, int line, const char *f,
            ...) {
@@ -13,6 +13,7 @@ int __log_output__(LOG_LEVEL level, const char *file_name, int line, const char 
   va_start(args, f);
   vsnprintf(msg, sizeof(msg), f, args);
   va_end(args);
+
 
   if (level >= LOG_LEVEL::LOG_LEVEL_ERR && level <= CUR_LEVEL) {
     printf("%s : in file: %s:%d:\n", msg, file_name, line);
